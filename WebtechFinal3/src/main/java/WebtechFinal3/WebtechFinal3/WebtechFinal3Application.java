59d47633321b4c3b8ec3d4f766ab0fabcc73dc0e
Corvus <corvin.jaenike@student.htw-berlin.de>
2023-10-22 21:57:23 +0200

    commit1

---
+++ WebtechFinal3/src/main/java/WebtechFinal3/WebtechFinal3/WebtechFinal3Application.java
@@ -0,0 +1,13 @@
+package WebtechFinal3.WebtechFinal3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechFinal3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebtechFinal3Application.class, args);
+	}
+
+}
---
+++ WebtechFinal3/src/test/java/WebtechFinal3/WebtechFinal3/WebtechFinal3ApplicationTests.java
@@ -0,0 +1,13 @@
+package WebtechFinal3.WebtechFinal3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechFinal3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
