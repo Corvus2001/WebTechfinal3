785a3253a713bf83bd5802cc7fd3bae7d89aa1ca
Zeyn <zeynabi@gmx.de>
2023-11-06 00:13:50 +0100

    commit

---
+++ WebtechFinal3/src/main/java/WebtechFinal3/WebtechFinal3/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package WebtechFinal3.WebtechFinal3.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model){
+        return "home";
+    }
+}
