ef233f86508360e895038b2e5aa3db063b249a77
Zeyn <zeynabi@gmx.de>
2023-11-08 10:56:19 +0100

    konfiguriert

--- WebtechFinal3/src/main/java/WebtechFinal3/WebtechFinal3/HomeController.java
+++ WebtechFinal3/src/main/java/WebtechFinal3/WebtechFinal3/HomeController.java
@@ -1,21 +1,20 @@
 package WebtechFinal3.WebtechFinal3;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
+@Controller
+public class HomeController {
     @GetMapping("/")
     public String home(Model model){
         return "home";
     }
 
-
-
-    }
+}
 
 
 
